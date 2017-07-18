907e869194f0fe9974cbf27a0d2b478ae84fd7ed
sue martinez <sueherrera30@gmail.com>
2017-07-18 19:59:24 +0000

    tag 0.01

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,12 +1,37 @@
-var registroBtn = /*document.getElementByid("registrame-btn");
-
-registroBtn.addEventListener("click",registrarse);
-
-function registrarse(){
-  
-}*/
+var url= "http://localhost:3000/registerNumber";
 
 
+/* VERSION UNO :()
+function obtenerInfo() {
+	$.getJSON(url,function(numero){		
+		numero.forEach(verNumeros);
+	});
+}
+
+function verNumeros(numeros){
+	console.log(numeros)
+}
+*/
+
+/*VERSION DOS CON XMLHTTPrequest y promise*/
+function pedirInfo(url) {
+	return new Promise(function (resolve, reject) {
+		var ajax = new XMLHttpRequest();
+		ajax.open("GET", url);
+		ajax.send();
+		ajax.onreadystatechange = function () {
+			if (ajax.readyState == 4) {
+				resolve(JSON.parse(ajax.responseText));
+			}
+		}
+	})
+}
+
+/*pedirInfo(aqui va mi json no se que aqui )
+.then.... */
+
+
+	
 
 
 
