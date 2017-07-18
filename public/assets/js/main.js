b0af1bd9ac9cf24501b2ceb34fb1840259a5a6e5
sue martinez <sueherrera30@gmail.com>
2017-07-18 18:52:22 +0000

    maquetacion parte registro

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,1 +1,14 @@
+var registroBtn = /*document.getElementByid("registrame-btn");
+
+registroBtn.addEventListener("click",registrarse);
+
+function registrarse(){
+  
+}*/
+
+
+
+
+
+/*carousel*/
 $('.carousel.carousel-slider').carousel({fullWidth: true});
